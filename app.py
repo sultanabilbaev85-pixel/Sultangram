4d3b09fe03030ec12638ca66a9b5871d4c5462d7
sultanabilbaev85-pixel <sultanabilbaev85@gmail.com>
2026-05-03 10:40:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import os
+from datetime import datetime
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sultangram-super-secret'
-# База данных будет сохраняться в файл sultangram.db
+app.config['SECRET_KEY'] = 'sultangram-boss-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sultangram.db'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
-# Создаем папку для загрузок, если её нет
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
+# Авто-создание папки для фото
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
@@ -26,9 +25,10 @@ class User(UserMixin, db.Model):
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(500))
+    content = db.Column(db.String(1000))
     image_path = db.Column(db.String(200))
     username = db.Column(db.String(150))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -37,7 +37,7 @@ def load_user(user_id):
 @app.route('/')
 @login_required
 def index():
-    messages = Message.query.all()
+    messages = Message.query.order_by(Message.timestamp.asc()).all()
     return render_template('index.html', user=current_user, messages=messages)
 
 @app.route('/send', methods=['POST'])
@@ -46,11 +46,9 @@ def send():
     text = request.form.get('text')
     file = request.files.get('photo')
     filename = None
-    
     if file and file.filename != '':
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    
     if text or filename:
         new_msg = Message(content=text, image_path=filename, username=current_user.username)
         db.session.add(new_msg)
@@ -69,19 +67,20 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        # Хешируем пароль для безопасности
         hash_pw = generate_password_hash(request.form.get('password'))
         new_user = User(username=request.form.get('username'), password=hash_pw)
         try:
             db.session.add(new_user)
             db.session.commit()
             return redirect(url_for('login'))
-        except:
-            return "Этот ник уже занят!"
+        except: return "Ошибка: Ник занят"
     return render_template('register.html')
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    with app.app_context(): db.create_all()
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
